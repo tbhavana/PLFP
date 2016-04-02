2cbd94780ebb16b8ecbe29d5c6251611e83a6c02
tbhavana <bhavanatangirala25@gmail.com>
2016-04-02 15:16:58 +0530

    Exception handling added

--- Assignments/Assignment-3/calculator/src/calculator/core.clj
+++ Assignments/Assignment-3/calculator/src/calculator/core.clj
@@ -1,6 +1,7 @@
 (ns calculator.core
   (:gen-class))
 
+
 (defn String->Number [str]
   (let [n (read-string str)]
        (if (number? n) n nil)))
@@ -14,8 +15,8 @@
 	(require '[clojure.string :as str])
 	(println "Enter:")
 	 (def elements (clojure.string/split (read-line) #" "))
-	 (def op1 (String->Number (get elements 0))) 		 
-	 (def op2 (String->Number (get elements 2)))	 
+	 (def op1 (try (String->Number (get elements 0)) (catch Exception e (println (str "Please follow the usage"))))) 		 
+	 (def op2 (try (String->Number (get elements 2)) (catch Exception e (println (str "Please follow the usage")))))	 
 	 (def operation (get elements 1))
 (if (not= op1 nil)
 	(if (not= op2 nil)	
@@ -23,13 +24,13 @@
 	"+"  (println (+ op1 op2)) 	
 	"-"  (println (- op1 op2))
 	"*"  (println (* op1 op2))	
-	"/"  (println (quot op1 op2))
+	"/"  (try (println (double (/ op1 op2)))(catch Exception e (println (str "caught exception: "(.getMessage e)))))
 	"default" 
 )
-(println "Invalid input")
+(println "Invalid operation")
 
 )
-(println "Invalid operands")
+(println "Invalid input")
 )
 )
 
